e1932c0a8dc9c43ab1ff114a72fe605195142987
Matt Chalmers <matt@barefootnomads.com>
2021-03-23 01:04:12 +1100

    name app in urls

--- comments_extension/urls.py
+++ comments_extension/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from .views.moderation import edit, edit_done
 
+
+app_name = "comments_extension"
+
 urlpatterns = [
     path("edit/<int:comment_id>/", edit, name="comments-edit"),
     path("edited/", edit_done, name="comments-edit-done"),
